d05f912a7d46cbbcf4acdfec530d1c27f00f64d9
Mauro <SotomayorMauro122@gmail.com>
2026-09-04 21:55:10 -0400

    Agrega pagina nosotros: vista horaria_y_guia

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalogo.urls')),
+    path('nosotros/', include('nosotros.urls')),
 ]
